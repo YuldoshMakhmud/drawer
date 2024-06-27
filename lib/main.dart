78fba7f6bcb045be7e9a50f2983acc771b2ee8b5
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-06-27 23:26:44 +0900

    drawer

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:lesson4/home_page.dart';
+
+class DrawerLesson extends StatelessWidget {
+  const DrawerLesson({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
